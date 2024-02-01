360a2c42e77bc30bc7de5e8928621cf1801e377b
Levente Batuska <mr.levente97@gmail.com>
2024-02-01 15:43:33 +0100

    add mason-null-ls and preinstall LSPs, Linters etc

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -8,7 +8,7 @@ end)
 
 require('mason').setup({})
 require('mason-lspconfig').setup({
-  ensure_installed = {},
+  ensure_installed = { },
   handlers = {
     lsp_zero.default_setup,
   },
---
+++ after/plugin/mason-null-ls.lua
@@ -0,0 +1,3 @@
+require("mason-null-ls").setup({
+    ensure_installed = { "ansible-language-server", "ansiblels", "ansible-lint", "bash-debug-adapter", "bash-language-server", "bashls", "clang-format", "clangd", "cmake-language-server", "cmake", "cmakelang", "cmakelint", "omnisharp", "python-lsp-server", "pylsp", "shellcheck", "shfmt" }
+})
