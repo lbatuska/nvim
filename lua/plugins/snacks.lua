5b1be6732e5b96ceaca21bd5f200b2dcd04eeab9
Levente Batuska <mr.levente97@gmail.com>
2024-12-14 12:04:13 +0100

    Sat Dec 14 12:04:13 PM CET 2024

---
+++ lua/plugins/snacks.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "folke/snacks.nvim",
+    opts = {
+      terminal = {
+        win = {
+          position = "float",
+        },
+      },
+    },
+  },
+}
+-- https://github.com/LazyVim/LazyVim/issues/4740
