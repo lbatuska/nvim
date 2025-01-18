18d47bb02151bfe2553281c6a4485359b45ef509
Levente Batuska <mr.levente97@gmail.com>
2025-01-18 14:48:58 +0100

    Sat Jan 18 02:48:58 PM CET 2025

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,3 +2,21 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.opt.clipboard = { "unnamedplus" }
+
+if vim.env.SSH_TTY then
+  vim.opt.clipboard:append("unnamedplus")
+  local function paste()
+    return vim.split(vim.fn.getreg(""), "\n")
+  end
+  vim.g.clipboard = {
+    name = "OSC 52",
+    copy = {
+      ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+      ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+    },
+    paste = {
+      ["+"] = paste,
+      ["*"] = paste,
+    },
+  }
+end
