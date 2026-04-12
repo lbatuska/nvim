8e0ad2876e87498f975a793fe260c1a4f16d5e9b
Levente Batuska <mr.levente97@gmail.com>
2026-04-12 09:53:12 +0200

    Sun Apr 12 09:53:12 AM CEST 2026

--- lua/plugins/go.lua
+++ lua/plugins/go.lua
@@ -1,14 +1,28 @@
-return {
-  "ray-x/go.nvim",
-  dependencies = {
-    "ray-x/guihua.lua",
-    "neovim/nvim-lspconfig",
-    "nvim-treesitter/nvim-treesitter",
-  },
-  config = function()
-    require("go").setup()
-  end,
-  event = { "CmdlineEnter" },
-  ft = { "go", "gomod" },
-  build = ':lua require("go.install").update_all_sync()',
-}
+if true then
+  return {}
+else
+  return {
+    "ray-x/go.nvim",
+    dependencies = {
+      "ray-x/guihua.lua",
+      "neovim/nvim-lspconfig",
+      { "nvim-treesitter/nvim-treesitter", branch = "main" },
+    },
+    opts = function()
+      require("go").setup(opts)
+      local format_sync_grp = vim.api.nvim_create_augroup("GoFormat", {})
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        pattern = "*.go",
+        callback = function()
+          require("go.format").goimports()
+        end,
+        group = format_sync_grp,
+      })
+      return {
+        -- lsp_keymaps = false,
+      }
+    end,
+    event = { "CmdlineEnter" },
+    ft = { "go", "gomod" },
+  }
+end
